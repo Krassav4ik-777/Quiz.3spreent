a004b677f2db4d5597e8316e1a072199f01cf0e1
Albert <Krassav4ik7777@yandex.ru>
2023-10-06 21:00:38 +0300

    Initial Commit

---
+++ Quiz.3spreent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz.3spreent
+//
+//  Created by KraSSavchiK on 06.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
