e9633a8bf7364a4ee0bd84b21baacd2b69989f59
Albert <Krassav4ik7777@yandex.ru>
2023-10-10 16:48:38 +0300

    Законченный проект с счётчиком.

--- Quiz.3spreent/ViewController.swift
+++ Quiz.3spreent/ViewController.swift
@@ -8,13 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var buttonCounter: UILabel!
+    var namber = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        buttonCounter.text = "\(namber)"
         // Do any additional setup after loading the view.
         
     }
-
-
+    @IBAction func buttonTouch(_ sender: Any) {
+        namber += 1
+        buttonCounter.text = "Значение счётчика: \(namber)"
+    }
 }
-
